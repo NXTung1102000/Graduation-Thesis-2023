234e78f7da35f514317cb7478fad90d6d941aa2c
NXTung1102000 <nxtung1102000@gmail.com>
2023-05-06 01:14:20 +0700

    add constant for type, class of exam

--- web-app/src/constant/name.tsx
+++ web-app/src/constant/name.tsx
@@ -15,3 +15,29 @@ export enum UrlSourceExam {
   ON_LUYEN = 'https://www.onluyen.vn/',
   TIM_DAP_AN = 'https://timdapan.com/',
 }
+
+export enum TypeExam {
+  MidTerm1 = 'Giữa kỳ 1',
+  MidTerm2 = 'Giữa kỳ 2',
+  EndTerm1 = 'Cuối kỳ 1',
+  EndTerm2 = 'Cuối kỳ 2',
+  Demo = 'Thi thử THPTQG',
+  // Advanced = 'Học sinh giỏi',
+}
+
+export const typeExamList = [
+  TypeExam.MidTerm1,
+  TypeExam.MidTerm2,
+  TypeExam.EndTerm1,
+  TypeExam.EndTerm2,
+  TypeExam.Demo,
+  //   TypeExam.Advanced,
+];
+
+export enum ClassExam {
+  Class10 = 'Lớp 10',
+  Class11 = 'Lớp 11',
+  Class12 = 'Lớp 12',
+}
+
+export const classList = [ClassExam.Class10, ClassExam.Class11, ClassExam.Class12];
